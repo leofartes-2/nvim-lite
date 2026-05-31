f639ec44de6c2e0f7caafa1b16063f89a7e2a760
Radley Sidwell-Lewis <radley.lewis@proton.me>
2026-05-31 12:56:14 +0700

    chore: set obsidian note paths based on os-release

--- init.lua
+++ init.lua
@@ -495,26 +495,34 @@ end
 
 setup_treesitter()
 
-local function setup_obsidian()
-	if vim.fn.isdirectory(vim.fn.expand("~/Notes/")) == 0 then
-		return
-	end
-	require("obsidian").setup({
-		legacy_commands = false,
-		workspaces = { { name = "Notes", path = vim.fn.expand("~/Notes/") } },
-		picker = { name = "fzf-lua" },
-	})
+local function get_notes_path()
+  local os_release = vim.fn.system("cat /etc/os-release")
+  if os_release:match("Artix") then
+    return vim.fn.expand("~/Documents/Notes")
+  elseif os_release:match("Ubuntu") then
+    return "/mnt/c/Users/Rad/Documents/Notes"
+  else
+    error("Unsupported OS: no notes path configured")
+  end
+end
 
-	vim.keymap.set("n", "<leader>nn", function()
-		vim.cmd("Obsidian workspace")
-		vim.defer_fn(function()
-			vim.cmd("Obsidian new")
-		end, 500)
-	end, { desc = "New note" })
-	vim.keymap.set("n", "<leader>nf", "<cmd>Obsidian quick_switch<cr>", { desc = "Find note" })
-	vim.keymap.set("n", "<leader>ns", "<cmd>Obsidian search<cr>", { desc = "Search notes" })
-	vim.keymap.set("n", "<leader>nt", "<cmd>Obsidian today<cr>", { desc = "Today's daily note" })
-	vim.keymap.set("n", "<leader>nw", "<cmd>Obsidian workspace<cr>", { desc = "Switch workspace" })
+local function setup_obsidian()
+  require("obsidian").setup({
+    legacy_commands = false,
+    workspaces = { { name = "Notes", path = get_notes_path() } },
+    picker = { name = "fzf-lua" },
+  })
+
+  vim.keymap.set("n", "<leader>nn", function()
+    vim.cmd("Obsidian workspace")
+    vim.defer_fn(function()
+      vim.cmd("Obsidian new")
+    end, 500)
+  end, { desc = "New note" })
+  vim.keymap.set("n", "<leader>nf", "<cmd>Obsidian quick_switch<cr>", { desc = "Find note" })
+  vim.keymap.set("n", "<leader>ns", "<cmd>Obsidian search<cr>",       { desc = "Search notes" })
+  vim.keymap.set("n", "<leader>nt", "<cmd>Obsidian today<cr>",        { desc = "Today's daily note" })
+  vim.keymap.set("n", "<leader>nw", "<cmd>Obsidian workspace<cr>",    { desc = "Switch workspace" })
 end
 
 setup_obsidian()
